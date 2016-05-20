aae16b6ed1d759ceb50a1da3117a4178ccaa0733
Albin Stjerna <albin.stjerna@gmail.com>
2016-05-20 18:00:23 +0200

    Implement string conversion + ingredient lists.

--- kokbok/model.py
+++ kokbok/model.py
@@ -21,10 +21,13 @@ class Ingredient:
             VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
             arglist = (self.name, self.price, self.energy, self.fat, self.protein,
                         self.carbohydrate, self.gramspermilliliter, self.gramsperunit)
-            print(execute_one(query, arglist))
+            self._id = execute_one(query, arglist)
 
 class Recipe:
-    def __init__(self, title, cook_time_prep, cook_time_cook, dimension, description, version, _id = None):
+    def __init__(self, title, cook_time_prep, cook_time_cook,
+                 dimension, description, version, ingredient_lists,
+                 author, instruction, comments, pictures, _id = None):
+
         self.title = title
         self.cook_time_prep = cook_time_prep
         self.cook_time_cook = cook_time_cook
@@ -32,7 +35,13 @@ class Recipe:
         self.description = description
         self.version = version
         self._id = _id
-        print(self._id)
+
+        self.ingredient_lists = ingredient_lists
+
+        self.author = author
+        self.instruction = instruction
+        self.comments = comments
+        self.pictures = pictures
 
     def save(self):
         if self._id == None:
@@ -42,14 +51,46 @@ class Recipe:
             arglist = (self.title, self.cook_time_prep, self.cook_time_cook,
                        self.dimension, self.description, self.version)
 
-            print(execute_one(query, arglist))
+            self._id = execute_one(query, arglist)
+
+class IngredientList:
+
+    def __init__(self, ingredients, title, _id=None):
+        self.ingredients = ingredients
+        self.title = title
+        self._id = _id
+
+    def save(self):
+        if self._id == None:
+            query = """INSERT INTO IngredientList (Title)
+            VALUES (%s) """
+
+            self._id = execute_one(query, [self.title])
+
+            for ingredient in self.ingredients:
+                query = """INSERT INTO IngredientList_Ingredient
+                      (IngredientListID, IngredientID, PrepNotes,
+                       Magnitude, Unit)
+                       VALUES(%s, %s, 1, 1, 1)"""
+
+                arglist = [self._id, ingredient._id]
+
+                execute_one(query, arglist)
+
+    def __str__(self):
+        s = ("Ingredients: %s\n"
+             "%s") % (self.title, str(self.ingredients))
+
+        return s
 
 def execute_one(query, arglist):
     with MySQLdb.connect(**conf.db) as cursor:
         cursor.execute(query, arglist)
-        return cursor.fetchall()
+
+        cursor.execute("SELECT LAST_INSERT_ID()")
+        return cursor.fetchone()[0]
 
 def execute_many(query, arglist):
     with MySQLdb.connect(**conf.db) as cursor:
         cursor.execute_many(query, arglist)
-        return cursor.fetchall()
+        return cursor.fetchone()
